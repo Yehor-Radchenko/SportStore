1eff0bae66d67679c13e9674a028b1e1bff3a673
Yehor Radchenko <y.p.radchenko@gmail.com>
2024-05-25 12:28:32 +0300

    Add project files.

---
+++ ServerApp/Models/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerApp.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasMany<Rating>(p => p.Ratings)
+                .WithOne(r => r.Product)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Product>()
+                .HasOne<Supplier>(p => p.Supplier)
+                .WithMany(s => s.Products)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
